00d12a13cfd901a6fdc6593a3edd131c3a60a7f9
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2026-01-15 13:32:09 +0530

    fix

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { connectDB } from "../../../../../lib/mongodb";
-import { Item } from "../../../../../models/Item";
-import { requireRole, logTransaction } from "../../../../../lib/auth";
+import { connectDB } from "../../../../lib/mongodb";
+import { Item } from "../../../../models/Item";
+import { requireRole, logTransaction } from "../../../../lib/auth";
 
 export async function PUT(
   req: NextRequest,
